4f39b5f5cc0057d54f84869a4b8aa12ad11e19de
ahmad hasan khan <er.ahmad.hassan@gmail.com>
2014-03-01 19:18:42 +0530

    little more changes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,31 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+var map = null;
+var geocoder = null;
+
+function initialize() {
+    if (GBrowserIsCompatible()) {
+        map = new GMap2(document.getElementById("map_canvas"));
+        map.setCenter(new GLatLng(28.655812600000000000, 77.241952200000010000), 13);
+        geocoder = new GClientGeocoder();
+    }
+}
+
+function showAddress(address) {
+    if (geocoder) {
+        geocoder.getLatLng(
+            address,
+            function (point) {
+                if (!point) {
+                    alert(address + " not found");
+                } else {
+                    map.setCenter(point, 13);
+                    var marker = new GMarker(point);
+                    map.addOverlay(marker);
+                    marker.openInfoWindowHtml(address);
+                }
+            }
+        );
+    }
+}
