7edff1ce060d35cb74263c697ebbe2bc29785d79
ahmad hasan khan <er.ahmad.hassan@gmail.com>
2014-03-01 18:48:49 +0530

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Placesearch::Application.config.secret_key_base = '2cce2cb02b1250e7663f84fed3ddab42b61b89d4a6896c0d4c6253eda5203696692a35c50b13326ab867ec3c0fc67cf4fe3fd15edf62812e7ae9312c7571c5e5'
